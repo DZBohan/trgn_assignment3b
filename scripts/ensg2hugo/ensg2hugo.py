6ad1365fea28cadb9ceb4afca1fc9d53bb2f8a99
DZBohan <781783051@qq.com>
2021-09-24 01:58:18 -0700

    Submitting

--- scripts/ensg2hugo/ensg2hugo.py
+++ scripts/ensg2hugo/ensg2hugo.py
@@ -1,16 +1,18 @@
 #In[]
 #!/usr/bin/python3
+import sys
 import re
 import pandas as pd
 ens2gene={}
 
-with open("gencode.v38.basic.annotation.gtf", 'r') as file:
+with open('gencode.v38.basic.annotation.gtf', 'r') as file:
     for line in file:
          matches=re.findall('.*gene_id "(.*?)".*gene_name "(.*?)";',line)
          if matches:
              ens2gene[matches[0][0].split('.')[0]]=matches[0][1]
 
-csv = pd.read_csv("expres.anal.csv",index_col=0)
+csv_filename = sys.argv[1]
+csv = pd.read_csv(csv_filename,index_col=0)
 def exchange_gene_name (id):
     id_simple=id.split('.')[0]
     if id_simple in ens2gene.keys():
